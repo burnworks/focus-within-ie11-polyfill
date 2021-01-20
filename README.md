33688bf374db624123202e5f26591f5df57088c2
burnworks <yoshiki@burnworks.com>
2021-01-20 23:14:59 +0900

    first-commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # focus-within-ie11-polyfill
- :focus-within Pseudo-Class Polyfill for IE11
+
+:focus-within Pseudo-Class Polyfill for IE11
+
+IE11 で :focus-within 疑似クラスを使用するための JavaScript です。
+
+## 使い方
+
+focus-within-ie11-polyfill.js を head 要素内などで読み込んでください。その後、`:focus-within` 疑似クラスを使用したい要素に `is-dropdown`（初期値）という class 名を附与すれば、その要素がフォーカスを持った時に、`focus-is-active`（初期値）という class が附与されます。
+
+`is-dropdown` と `focus-is-active` はスクリプト内の「対象要素と附与する class 名の設定」部分で設定していますので、自由に変更してください。
+
+## デモ
+
+`demo/sample-code.html` にサンプルソースがありますので参考まで。
+
+## その他
+
+本当は IE11 など、:focus-within 疑似クラスに対応しない環境でのみ動作するようにした方がよいと思いますが、現時点でそういう処理は入れていません。改変はご自由にどうぞ。
---
+++ src/focus-within-ie11-polyfill.js
@@ -0,0 +1,45 @@
+'use strict'
+    ; (function () {
+        // 対象要素と附与する class 名の設定
+        const focusItemClass = '.is-dropdown';
+        const focusActiveClass = 'focus-is-active';
+
+        // 対象要素を取得
+        const focusItems = document.querySelectorAll(focusItemClass);
+
+        // 取得した対象要素にイベントリスナーを登録
+        Array.prototype.forEach.call(focusItems, function (e) {
+            e.addEventListener('focusin', focusinListener);
+            e.addEventListener('focusout', focusoutListener);
+        });
+
+        // 対象要素内にフォーカスがある場合に class を附与
+        function focusinListener(event) {
+            event.target.closest(focusItemClass).classList.add(focusActiveClass);
+        };
+
+        // 対象要素からフォーカスが外れたら class を削除
+        function focusoutListener(event) {
+            if (!document.activeElement.classList.contains('e')) {
+                Array.prototype.forEach.call(focusItems, function (e) {
+                    e.classList.remove(focusActiveClass);
+                });
+            }
+        };
+
+        // IE11 で closest が動作しないので Polyfill
+        if (!Element.prototype.matches) {
+            Element.prototype.matches = Element.prototype.msMatchesSelector ||
+                Element.prototype.webkitMatchesSelector;
+        }
+        if (!Element.prototype.closest) {
+            Element.prototype.closest = function (s) {
+                let el = this;
+                do {
+                    if (el.matches(s)) return el;
+                    el = el.parentElement || el.parentNode;
+                } while (el !== null && el.nodeType === 1);
+                return null;
+            };
+        }
+    }());
